711e03055386f267f37cb65fa0f7ff8caa472240
Loick Emadisson <146085343+KiritoEM@users.noreply.github.com>
2025-11-23 17:41:54 +0300

    Time: 24 ms (4.73%) | Memory: 62.1 MB (6.83%) - LeetSync

---
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)  
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+var flatten = function (root) {
+    let nodesList = [];
+
+    const preOrder = (node, list) => {
+        if (!node) return;
+
+        list.push(node);
+
+        //left
+        preOrder(node.left, list);
+
+        //right
+        preOrder(node.right, list);
+    }
+
+    preOrder(root, nodesList);
+
+    console.log("nodesList: ", nodesList);
+
+    for (let i = 0; i < nodesList.length - 1; i++) {
+        nodesList[i].left = null;
+        nodesList[i].right = nodesList[i + 1];
+    }
+};
